d7a4349dd6259af8e1ef46c8d4ce412b30e341ca
Yury Shkoda <shkoda.yuri@gmail.com>
2021-03-16 15:31:30 +0300

    fix(status-bar): removed 'Run SAS code' button

--- src/commands/execute-code/ExecuteCodeCommand.ts
+++ src/commands/execute-code/ExecuteCodeCommand.ts
@@ -18,7 +18,6 @@ import { getTimestamp } from './internal/utils'
 
 export class ExecuteCodeCommand {
   private outputChannel: OutputChannel
-  private runSasCodeButton: StatusBarItem
 
   constructor(private context: ExtensionContext) {
     this.outputChannel = window.createOutputChannel('SASjs')
@@ -30,19 +29,6 @@ export class ExecuteCodeCommand {
       () => this.executeCode()
     )
     this.context.subscriptions.push(executeCodeCommand)
-
-    this.initialiseRunSasCodeButton()
-  }
-
-  private initialiseRunSasCodeButton = () => {
-    this.runSasCodeButton = window.createStatusBarItem(
-      StatusBarAlignment.Right,
-      100
-    )
-    this.runSasCodeButton.text = '$(notebook-execute) Run SAS code'
-    this.runSasCodeButton.command = 'sasjs-for-vscode.executeCode'
-    this.runSasCodeButton.show()
-    this.context.subscriptions.push(this.runSasCodeButton)
   }
 
   private executeCode = async () => {
@@ -63,7 +49,6 @@ export class ExecuteCodeCommand {
       debug: true
     })
 
-    this.runSasCodeButton.text = '$(sync~spin) Running your SAS code'
     adapter
       .executeScriptSASViya(
         'vscode-test-exec',
@@ -72,13 +57,6 @@ export class ExecuteCodeCommand {
         accessToken
       )
       .then(async (res) => {
-        this.runSasCodeButton.text =
-          '$(notebook-state-success) SAS code executed successfully!'
-
-        const timeout = setTimeout(() => {
-          this.runSasCodeButton.text = '$(notebook-execute) Run SAS code'
-          clearTimeout(timeout)
-        }, 3000)
         await createAndOpenLogFile(res.log)
 
         this.outputChannel.append(JSON.stringify(res, null, 2))
@@ -87,12 +65,6 @@ export class ExecuteCodeCommand {
         const { log } = e
         await createAndOpenLogFile(log)
 
-        this.runSasCodeButton.text =
-          '$(notebook-state-error) SAS code execution failed'
-        const timeout = setTimeout(() => {
-          this.runSasCodeButton.text = '$(notebook-execute) Run SAS code'
-          clearTimeout(timeout)
-        }, 3000)
         this.outputChannel.append(JSON.stringify(e, null, 2))
       })
   }
