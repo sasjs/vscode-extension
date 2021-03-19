26310f1069c6bf31f6742c946f859b56e53af24e
Krishna Acondy <kc.acondy@outlook.com>
2021-03-19 08:35:56 +0000

    chore(*): rename variable

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,13 @@
 import * as vscode from 'vscode'
 import { ExecuteCodeCommand } from './commands/execute-code/ExecuteCodeCommand'
 import { ExecutingCodeCommand } from './commands/execute-code/ExecutingCodeCommand'
-import { FormatCommand } from './commands/format/FormatCommand'
 
 export function activate(context: vscode.ExtensionContext) {
   const executeCodeCommand = new ExecuteCodeCommand(context)
   executeCodeCommand.initialise()
 
-  const executeCodeNewCommand = new ExecutingCodeCommand(context)
-  executeCodeNewCommand.initialise()
+  const executingCodeCommand = new ExecutingCodeCommand(context)
+  executingCodeCommand.initialise()
 }
 
 export function deactivate() {}
