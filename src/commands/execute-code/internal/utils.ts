c18661ac9051fde5a1a21f4014d1110d248dd3c0
Sabir Hassan <sabirbk06@gmail.com>
2022-11-01 20:40:58 +0500

    chore: move getTimeStamp utility from internal to utils folder
    add utility function to check if sasjs server is running in sever mode
    add utility method for opening .sasjsrc file

--- src/commands/execute-code/internal/utils.ts
+++
@@ -1,7 +0,0 @@
-import { timestampToYYYYMMDDHHMMSS } from '@sasjs/utils/time'
-
-export const getTimestamp = () =>
-  timestampToYYYYMMDDHHMMSS()
-    .replace(/ /g, '')
-    .replace(/\//g, '')
-    .replace(/:/g, '')
