448a6c9ebf8e990e69a1fa1754a10ce7f349a4c4
Sabir Hassan <sabirbk06@gmail.com>
2023-04-12 22:57:17 +0500

    feat: look for global .sasjslint file before using default setting

--- src/utils/getLintConfig.ts
+++ src/utils/getLintConfig.ts
@@ -1,16 +1,22 @@
 import * as path from 'path'
+import * as os from 'os'
 import { workspace } from 'vscode'
 import { LintConfig, DefaultLintConfiguration } from '@sasjs/lint'
 import { readFile } from './file'
+import { fileExists } from '@sasjs/utils'
 
 export const getLintConfig = async () => {
   let config = DefaultLintConfiguration
 
-  const lintConfigPath = path.join(process.projectDir, '.sasjslint')
+  let lintConfigPath = path.join(os.homedir(), '.sasjslint')
+
+  if (await fileExists(path.join(process.projectDir, '.sasjslint'))) {
+    lintConfigPath = path.join(process.projectDir, '.sasjslint')
+  }
 
   await readFile(lintConfigPath)
     .then((content) => {
-      config = { ...config, ...JSON.parse(content) }
+      config = JSON.parse(content)
     })
     .catch(() => {
       const extConfig = workspace.getConfiguration('sasjs-for-vscode')
